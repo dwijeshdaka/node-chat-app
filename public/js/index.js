a9cf95eb7bb9e49e847833747e406f7cacb099d3
dwijesh daka <dwijesh.daka@gmail.com>
2018-12-29 00:38:33 +0530

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -16,17 +16,6 @@ socket.on('newMessage',function(message){
     jQuery('#messages').append(li);
 });
 
-socket.on('newLocationMessage',function(message){
-
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My Current Location</a>');
-
-    li.text(`${message.from}:`);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
-});
-
 // socket.emit('createMessage',{
 //     from:'Hello',
 //     text:'hi'
@@ -60,4 +49,15 @@ sendLocation.on('click',function(){
     },function(){
         alert('Unable to get location');
     })
+});
+
+socket.on('newLocationMessage',function(message){
+
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My Current Location</a>');
+
+    li.text(`${message.from}:`);
+    a.attr('href',message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
 });
